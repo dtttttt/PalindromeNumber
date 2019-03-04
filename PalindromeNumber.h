866bbf4d2df30629087da92b109d8ae665932dcf
dtttttt <47856979+dtttttt@users.noreply.github.com>
2019-03-04 22:15:20 +0800

    2th
    Runtime: 120 ms, faster than 97.17% of C++ online submissions for Palindrome Number.
    Memory Usage: 73.2 MB, less than 19.51% of C++ online submissions for Palindrome Number.
    效果不如从前好，但算法很简单，不如1th是因为这个要完全遍历，而1th只一半；

--- PalindromeNumber.h
+++ PalindromeNumber.h
@@ -1,17 +1,11 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-    std::string DataString;
+   std::string DataString;
 	DataString = std::to_string(x);
-	std::string::iterator i = DataString.begin();
-	std::string::iterator j = DataString.end() - 1;
-	for (; j>i;i++,j--)
-	{
-		if (*i!=*j)
-		{
-			return false;
-		}
-	}
-	return true;    
+	std::string DataStringResult;
+	DataStringResult = DataString;
+	reverse(DataString.begin(), DataString.end());
+	return(DataString == DataStringResult ? true : false);  
     }
 };
